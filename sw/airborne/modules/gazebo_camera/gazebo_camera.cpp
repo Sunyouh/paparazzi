4a898529f3745212b7e8c06dfeb0c6506e7498f2
sunyouh <sunyouh35@gmail.com>
2020-10-26 15:54:30 +0100

    initial commit of Gazebo camera module

---
+++ sw/airborne/modules/gazebo_camera/gazebo_camera.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) Sunyou Hwang <S.Hwang-1@tudelft.nl>
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/** @file "modules/gazebo_camera/gazebo_camera.c"
+ * @author Sunyou Hwang <S.Hwang-1@tudelft.nl>
+ * -
+ */
+
+extern "C" {
+#include "modules/gazebo_camera/gazebo_camera.h"
+}
+
+void gazebo_camera_init(void)
+{
+  // your init code here
+}
+
+void gazebo_camera_periodic(void)
+{
+  // your periodic code here.
+  // freq = 30.0 Hz
+}
+
+
