85ac0044b3c4fd40cc7fd7542976dd23812b79b4
sunyouh <sunyouh35@gmail.com>
2020-11-26 17:42:21 +0100

    CFD wind data importing module

---
+++ sw/ground_segment/python/wind_data_importer/wind_data_importer.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+
+"""/*
+ * Copyright (C) Sunyou Hwang <S.Hwang-1@tudelft.nl>
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+ Modified from mesonh.py
+"""
+
+
+from __future__ import absolute_import, print_function, division
+from wind_importer_openfoam import OpenFoamImporter
+import sys
+import signal
+import time
+# import socket
+# import struct
+# import cmath
+import os
+import numpy as np
+
+# Add pprz_home path
+PPRZ_HOME = os.getenv("PAPARAZZI_HOME", os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                                    '../../../..')))
+sys.path.append(PPRZ_HOME + "/var/lib/python")
+
+from pprzlink.ivy import IvyMessagesInterface
+from pprzlink.message import PprzMessage
+
+# M_IN_KM = 1000.
+
+# atm = None
+origin = np.array([0, 0, 0])
+# scale = np.array([1., 1/M_IN_KM, 1/M_IN_KM, 1/M_IN_KM])
+
+start_time = time.time()
+
+
+def get_wind(east, north, up):
+    # t = time.time() - start_time
+    # print("east :",east)
+    # print("north :",north)
+    # print("up :",up)
+    loc = np.array([east, north, up])
+    loc = loc + origin
+    # print("loc:",loc)
+    # TODO: import wind data
+    weast, wnorth, wup = importer.get_wind(loc)
+    # weast, wnorth, wup = np.random.rand()*3, np.random.rand()*3, np.random.rand()*2
+    return weast, wnorth, wup
+
+
+def ivy_request_callback(sender, msg, resp, *args, **kwargs):
+    """
+        Ivy Callback for Paparazzi Requests
+    """
+
+    if msg.msg_class == "ground" and msg.name == "WORLD_ENV_REQ":
+        return worldenv_cb(msg, resp)
+    else:
+        return None
+
+
+def worldenv_cb(ac_id, msg):
+    """
+        Callback for paparazzi WORLD_ENV requests
+        the response should be *ENU*
+    """
+    # request location (in meters)
+    east, north, up = float(msg.get_field(3)),\
+        float(msg.get_field(4)),\
+        float(msg.get_field(5))
+    weast, wnorth, wup = get_wind(east, north, up)
+    # print("wind_est:")
+    # print(weast)
+    # print(wnorth)
+    # print(wup)
+    msg_back=PprzMessage("ground", "WORLD_ENV")
+    msg_back.set_value_by_name("wind_east",weast)
+    msg_back.set_value_by_name("wind_north",wnorth)
+    msg_back.set_value_by_name("wind_up",wup)
+    msg_back.set_value_by_name("ir_contrast",400)
+    msg_back.set_value_by_name("time_scale",1)
+    msg_back.set_value_by_name("gps_availability",1)
+    ivy.send(msg_back,None)
+
+
+def signal_handler(signal, frame):
+    print('\nShutting down IVY...')
+    ivy.shutdown()
+    print("Done.")
+
+
+def main():
+    # parse arguments
+    import argparse as ap
+
+    argp = ap.ArgumentParser(description="Wind data provider "
+                             "for Paparazzi from CFD or potential flow simulation")
+
+    argp.add_argument("-p", "--type", required=False, default=0, type=int,
+                        help="Specify the type; 0=OpenFOAM, 1=PotentialFlow")
+
+    argp.add_argument("-f", "--file", required=False, default="/home/sunyou/tud/cfd/wind_test.csv",
+                      help="OpenFOAM result file in full path")
+
+    argp.add_argument("-t", "--time-step", required=False, type=int,
+                      help="Time step for importing dynamic/time-variant CFD simulation. "
+                      "Not required for steady-states.")
+
+    argp.add_argument("-x", "--origin-x", required=False, type=float,
+                      default=0.,
+                      help="Origin translation x.")
+    argp.add_argument("-y", "--origin-y", required=False, type=float,
+                      default=0.,
+                      help="Origin translation y.")
+    argp.add_argument("-z", "--origin-z", required=False, type=float,
+                      default=0.,
+                      help="Origin translation z.")
+    arguments = argp.parse_args()
+
+    print(arguments)
+
+    # register signal handler for ctrl+c to stop the program
+    signal.signal(signal.SIGINT, signal_handler)
+
+    global origin
+    origin = np.array([arguments.origin_x, arguments.origin_y, arguments.origin_z])
+
+    # build atmosphere simulation source
+    global importer
+    if arguments.type == 0:
+        importer = OpenFoamImporter()
+        importer.init_importer(arguments.file)
+#     elif arguments.type == 1:
+#         importer = PotentialFlowImporter()
+    else:
+        print("Please specify importer type")
+
+    # init ivy and register callback for WORLD_ENV_REQ and NPS_SPEED_POS
+    global ivy
+    ivy = IvyMessagesInterface("WindSimulationData")
+    ivy.subscribe(worldenv_cb,'(.* WORLD_ENV_REQ .*)')
+
+    # wait for ivy to stop
+    from ivy.std_api import IvyMainLoop  # noqa
+
+    signal.pause()
+
+
+if __name__ == '__main__':
+    main()
---
+++ sw/ground_segment/python/wind_data_importer/wind_importer_openfoam.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+
+"""/*
+ * Copyright (C) Sunyou Hwang <S.Hwang-1@tudelft.nl>
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+"""
+
+import numpy as np
+from scipy import interpolate
+
+
+class OpenFoamImporter:
+    def __init__(self):
+        self.cfd_points = None
+        self.cfd_u = None
+        self.interpolator_x = None
+        self.interpolator_y = None
+        self.interpolator_z = None
+        self.translate_origin = (0, 0, 0)
+        self.wind_data = None
+        self.count = 0
+
+    def init_importer(self, f_name, method="nearest"):
+        print("init importer")
+        self.read_csv(f_name)
+
+        # self.linear_interpolator = interpolate.RegularGridInterpolator(self.cfd_points, self.cfd_u, "linear")
+        # x_arr = (self.cfd_u[0], self.cfd_u[0], self.cfd_u[0])
+        # x_arr_2 = np.array(zip(x_arr))
+        # self.interpolator_x = interpolate.RegularGridInterpolator(self.cfd_points, x_arr, method=method)
+        # self.interpolator_y = interpolate.RegularGridInterpolator(self.cfd_points, self.cfd_u[1], method=method)
+        # self.interpolator_z = interpolate.RegularGridInterpolator(self.cfd_points, self.cfd_u[2], method=method)
+
+    def translate_field(self, x=0, y=0, z=0):
+        # self.cfd_points[0, :] += x
+        # self.cfd_points[1, :] += y
+        # self.cfd_points[2, :] += z
+        self.translate_origin += (x, y, z)
+
+    # def interpolate3d(self, x, y, z):
+    #     return self.linear_interpolator(np.array((x, y, z)))
+
+    # def interpolate3d(self, _loc):
+    #     loc = _loc - self.translate_origin
+    #     wind_x = self.interpolator_x(loc)
+    #     wind_y = self.interpolator_y(loc)
+    #     wind_z = self.interpolator_z(loc)
+    #     return wind_x, wind_y, wind_z
+
+    # paraview csv; P0, P1, P2, P_mag, U0, U1, U2, U_mag, eps, k, nut, p, block, p_id
+    def read_csv(self, f_name):
+        csv_data = np.genfromtxt(fname=f_name, delimiter=",")
+        # _pts = csv_data[1:, :3]
+        # print("pts: ", _pts.shape)
+        # _u = csv_data[1:, 4:7]
+        # print("u: ", _u.shape)
+        self.wind_data = np.concatenate((csv_data[1:, :3], csv_data[1:, 4:7]), axis=1)
+        # print(self.wind_data.shape)
+        # self.cfd_points = (_pts[:, 0], _pts[:, 1], _pts[:, 2])
+        # self.cfd_u = (_u[:, 0], _u[:, 1], _u[:, 2])
+        # print(len(self.cfd_points))
+        # print(len(self.cfd_u))
+
+    def get_wind(self, loc):
+        # loc: (east, north, up) == (y, x, z)
+        # self.count += 1
+        # print(self.count)
+        # print("get_wind ", self.wind_data.shape)
+        _x_nearest_arr = self.find_nearest(self.wind_data, loc[1], 0)
+        _y_nearest_arr = self.find_nearest(_x_nearest_arr, loc[0], 1)
+        _z_nearest_arr = self.find_nearest(_y_nearest_arr, loc[2], 2)
+        wind_data = list(_z_nearest_arr)[0]
+        """
+        OpenFoam x:North y:West z:Up
+        return *ENU*
+        """
+        # East North Up
+        return (-1*wind_data[4], wind_data[3], wind_data[5])
+
+    def find_nearest(self, arr, value, axis):
+        # print(value, axis)
+        err = np.abs(np.asarray(arr[:, axis]) - value)
+        # err_min = err.min()
+        # print("min_err: ", err_min)
+        idx = np.where(err == err.min())
+        return np.array(arr[idx, :])[0]
+
+
+    # def get_points(self, points, var, method='nearest'):
+    #     """ Get value of variable on points
+    #
+    #     Arguments:
+    #     points: a ndarray containing the point coordinates on the last
+    #     dimension
+    #     var: the name of the variable in the mesoNH file(s)
+    #     method: 'nearest' and 'linear' interpolation are currently supported
+    #     """
+    #     points = np.array(points)
+    #     p = self._apply_bounds(points)
+    #     caxes = tuple(range(p.ndim - 1))
+    #     bounds = zip(p.min(axis=caxes), p.max(axis=caxes))
+    #     interpolator = self._get_interpolator(bounds, var, method)
+    #     return interpolator(p, method).squeeze()
+    #
+    # def _get_interpolator(self, bounds, var, method="nearest"):
+    #     slice_indexes, coordinates = self._slicyfy(bounds)
+    #     values = self._get_var_values(var, slice_indexes)
+    #     ip = RegularGridInterpolator(coordinates, values, method)
+    #     return ip
+    #
+    # def _slicyfy(self, bounds):
+    #     slice_indexes = ()
+    #     coordinates = ()
+    #     for d, b in enumerate(bounds):
+    #         dslice = slice(find_le(self.grid_coordinates[d], b[0]),
+    #                        find_gt(self.grid_coordinates[d], b[1]) + 1)
+    #         slice_indexes += dslice,
+    #         coordinates += self.grid_coordinates[d][dslice],
+    #
+    #     return slice_indexes, coordinates
+    #
+    # def _get_var_values(self, var, idx=Ellipsis):
+    #     return self.data[var][idx]
+
+
+"""
+# Create coordinate pairs
+cartcoord = list(zip(x, y))
+
+
+X = np.linspace(min(x), max(x))
+Y = np.linspace(min(y), max(y))
+X, Y = np.meshgrid(X, Y)
+
+# Approach 1
+interp = scipy.interpolate.LinearNDInterpolator(cartcoord, z, fill_value=0)
+Z0 = interp(X, Y)
+"""
+
+
+def main():
+    print("This main fn is for testing")
+    fname = "/home/sunyou/tud/cfd/export_sample.csv"
+    importer = OpenFoamImporter()
+    importer.init_importer(fname)
+    wind = importer.get_wind((123, -50.5, 150))
+    print(wind)
+
+
+if __name__ == '__main__':
+    main()
